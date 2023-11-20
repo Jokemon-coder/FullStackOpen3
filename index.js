9c52fc4b8f3c70456b9ee60846828e7290741dfa
Jokemon-coder <Jokemon-coder@users.noreply.github.com>
2023-11-20 09:48:11 +0200

    Start of part 3 and task 3.1 done

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let persons = [
+    {
+        id: 1,
+        name: "Jacob Newman",
+        number: "123456"
+      },
+      {
+        id: 2,
+        name: "Petri Kallio",
+        number: "808080"
+      },
+      {
+        id: 3,
+        name: "Liisa Suo",
+        number: "676767"
+      }
+]
+
+app.get("/", (req, res) => {
+    res.send("<h1>HELLO</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+    res.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`We're running on ${PORT}`);
+});
